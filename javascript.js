e4080737e59ab981fb57f3c6cfb614034d4b8d5d
Denis Kelolli <deniskelolli@gmail.com>
2023-06-01 19:09:02 -0400

    Random RGB when hovering

--- javascript.js
+++ javascript.js
@@ -1,16 +1,36 @@
 const container = document.querySelector(".container");
+const button = document.querySelector(".button");
+
+const randomRgbColor = () => {
+    let r = Math.floor(Math.random() * 256); // Random between 0-255
+    let g = Math.floor(Math.random() * 256); // Random between 0-255
+    let b = Math.floor(Math.random() * 256); // Random between 0-255
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+  };
 
 
-for (let i = 0; i < 256; i++){
-    const div = document.createElement("div");
-    div.style.border = "2px solid black";
-    div.style.width = "30px";
-    div.style.height = "30px";
-    div.addEventListener("mouseover",() =>{
-        div.style.backgroundColor = "red";
-    })
 
-
-    container.appendChild(div);
+button.addEventListener("click", () => {
+    container.textContent = "";
+    userInput = parseInt(prompt("Enter a grid size: "));
     
-}
+    for (let i = 0; i < (userInput*userInput); i++){
+        if(userInput >= 100){
+            alert("ERROR. Enter a number less than 100");
+            break;
+            }
+        const div = document.createElement("div");
+        div.style.border = "2px solid black";
+        boxDims = (960/userInput) - 4;;
+        div.style.width = boxDims+"px";
+        div.style.height = boxDims+"px";
+        div.addEventListener("mouseover",() =>{
+            div.style.backgroundColor = randomRgbColor();
+        })
+        container.appendChild(div);
+    }
+
+})
+
+
+
